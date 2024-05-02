70027753d98fafb8258e3d15940e2db925e802c6
Pablo Rial Sartirana <65549425+rialpablojhotmailcom@users.noreply.github.com>
2024-05-02 11:42:05 +0100

    add basic methods and fields

--- src/main/java/org/example/CardDeck/Card.java
+++ src/main/java/org/example/CardDeck/Card.java
@@ -1,4 +1,31 @@
 package org.example.CardDeck;
 
+//A class used to Model a playing card.
+//It should have the following:
+//String suit: Unicode characters for heart, club, diamond, and spade.
+//String symbol: (2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A)
+//int value: (2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14)
+//Encapsulation should be used to hide the data
+//toString method for class description
+
+
 public class Card {
+
+    private final String[] suits = {"❤️", "♦️", "♣️", "♠️"};
+    private final String[] symbols = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+
+    //int value: (2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14)
+    //method that will return the value of each symbol, different for each game
+    public int[] getValues() {
+        return new int[0];
+    }
+
+    public String getSymbol() {
+        return "-1";
+    }
+
+
+    public String toString() {
+        return null;
+    }
 }
