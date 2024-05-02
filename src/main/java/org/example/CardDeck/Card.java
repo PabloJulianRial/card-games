f1060cf2e0a0046d928f5a364e6fe7da62ff161f
gordonmenzies <heymish96@gmail.com>
2024-05-02 13:44:54 +0100

    Deck And Card Relationship Established

--- src/main/java/org/example/CardDeck/Card.java
+++ src/main/java/org/example/CardDeck/Card.java
@@ -11,8 +11,19 @@ package org.example.CardDeck;
 
 public class Card {
 
-    private final String[] suits = {"❤️", "♦️", "♣️", "♠️"};
-    private final String[] symbols = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+
+
+    private final String symbol;
+    private final String suit;
+    private final int value;
+    //MAYBE
+    private boolean isDealt = false;
+
+    public Card(String symbol, String suit, int value) {
+        this.symbol = symbol;
+        this.suit = suit;
+        this.value = value;
+    }
 
     //int value: (2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14)
     //method that will return the value of each symbol, different for each game
@@ -21,9 +32,21 @@ public class Card {
     }
 
     public String getSymbol() {
-        return "-1";
+        return symbol;
     }
 
+    public String getCard() {
+        return symbol + suit;
+    }
+
+    public String getSuit() { return suit; }
+
+    public int getValue() {return value;}
+
+    // MAYBE
+    public void Deal() {
+        isDealt = true;
+    }
 
     public String toString() {
         return null;
